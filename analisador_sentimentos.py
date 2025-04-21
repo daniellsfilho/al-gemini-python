a1ab97c74162182f2981c5231d4f873d30f49585
Daniel Filho <daniel.2308@outlook.com>
2025-04-21 13:14:37 -0300

    aprimoramento analisador de sentimentos

--- analisador_sentimentos.py
+++ analisador_sentimentos.py
@@ -1,5 +1,6 @@
 import os
 import google.generativeai as ai
+from google.api_core.exceptions import NotFound
 from dotenv import  load_dotenv
 
 load_dotenv()
@@ -23,7 +24,9 @@ def escreverArquivo(caminho, conteudo):
     except IOError as ioError:
         print(f"Erro ao escrever arquivo: {ioError}")
 
-prompt_sistema = f"""
+def analisarSentimento(produto):
+
+    prompt_sistema = f"""
         Você é um analisador de sentimentos de avaliações de produtos.
         Escreva um parágrafo com até 50 palavras resumindo as avaliações e
         depois atribua qual o sentimento geral para o produto.
@@ -38,18 +41,29 @@ prompt_sistema = f"""
         Pontos fracos: lista com três bullets
     """
 
-llm_flash = ai.GenerativeModel(
-    model_name=MODELO,
-    system_instruction=prompt_sistema
-)
+    try:
+        llm_flash = ai.GenerativeModel(
+            model_name=MODELO,
+            system_instruction=prompt_sistema
+        )
 
-nome_arquivo = "Camisetas de algodão orgânico"
-caminho_arquivo = f"./dados/avaliacoes-{nome_arquivo}.txt"
+        caminho_arquivo = f"./dados/avaliacoes-{produto}.txt"
 
-prompt_usuario = carregarArquivo(caminho_arquivo)
+        prompt_usuario = carregarArquivo(caminho_arquivo)
 
-resposta = llm_flash.generate_content(prompt_usuario)
-print(f"Salvando arquivo resposta-{nome_arquivo}")
+        resposta = llm_flash.generate_content(prompt_usuario)
+        print(f"Salvando arquivo resposta-{produto}")
 
-caminho_arquivo_resposta = f"./dados/resposta-{nome_arquivo}"
-escreverArquivo(caminho_arquivo_resposta, resposta.text)
+        caminho_arquivo_resposta = f"./dados/resposta-{produto}"
+        escreverArquivo(caminho_arquivo_resposta, resposta.text)
+    except NotFound as e:
+        print(f"Erro ao carregar modelo: {e}")
+
+def main():
+    produtos = ["Camisetas de algodão orgânico", "Jeans feitos com materiais reciclados", "Maquiagem mineral"]
+
+    for produto in produtos:
+        analisarSentimento(produto)
+
+if __name__ == "__main__":
+    main()
